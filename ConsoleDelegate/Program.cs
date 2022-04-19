80a6def6e59c76bf1790350df21227f31bad6733
BesteSakar <bestesakarr@outlook.com>
2022-04-19 12:28:50 +0300

    ConsoleDelegate
    Çevre ve alan hesabı yapan metodlarımızı ana metodumuz içerisinde delegateden nesne türetip parametre geçirerek kullanma..

---
+++ ConsoleDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleDelegate
+{
+    internal class Program
+    {
+
+
+        delegate double Dikdortgen(double x, double y);
+
+
+        static void Main(string[] args)
+        {
+
+            Dikdortgen test = new Dikdortgen(alan);
+            Console.WriteLine("Dikdörtgenin Alanı :" + " " + test(3,8));
+            Console.ReadLine();
+           
+        }
+
+        static double cevre(double a,double b)
+        {
+
+            return 2 * (a + b);
+
+
+        }
+
+        static double alan(double a , double b)
+        {
+            return a * b;
+        }
+
+
+
+    }
+}
